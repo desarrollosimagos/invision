5aea0b349fcec13c146246251c608d01fd672e53
Jose <solorzano202009@gmail.com>
2018-01-15 10:08:10 -0400

    Actualización de los campos de monedas.

--- application/controllers/CResumen.php
+++ application/controllers/CResumen.php
@@ -21,6 +21,30 @@ class CResumen extends CI_Controller {
 		$data['capital_pendiente'] = $this->MResumen->capitalPendiente();
 		$data['capital_aprobado'] = $this->MResumen->capitalAprobado();
 		$this->load->view('resumen/resumen', $data);
+		
+		//~ function currencyConverter($from_Currency,$to_Currency,$amount) {
+			//~ $from_Currency = urlencode($from_Currency);
+			//~ $to_Currency = urlencode($to_Currency);
+			//~ $encode_amount = 1;
+			//~ $get = file_get_contents("https://www.google.com/finance/converter?a=$encode_amount&from=$from_Currency&to=$to_Currency");
+			//~ // $get = file_get_contents("http://www.xe.com/es/currencyconverter/convert/?Amount=1&From=USD&To=EUR");
+			//~ $get = explode("<span class=bld>",$get);
+			//~ $get = explode("</span>",$get[1]);
+			//~ $converted_currency = preg_replace("/[^0-9.]/", null, $get[0]);
+			//~ return $converted_currency;
+			//~ // return $get;
+		//~ }
+//~ 
+		//~ // change amount according to your needs
+		//~ $amount =10;
+		//~ // change From Currency according to your needs
+		//~ $from_Curr = "INR";
+		//~ // change To Currency according to your needs
+		//~ $to_Curr = "USD";
+		//~ $converted_currency=currencyConverter($from_Curr, $to_Curr, $amount);
+		//~ // Print outout
+		//~ echo $converted_currency;
+		
 		$this->load->view('footer');
 	}
 	
