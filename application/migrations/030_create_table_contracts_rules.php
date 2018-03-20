a54bbbbe1a1a7fab19b567d1a1cdeec5ad73be9b
Jose <solorzano202009@gmail.com>
2018-03-20 10:56:21 -0400

    Nueva tabla 'contracts_rules'.

---
+++ application/migrations/030_create_table_contracts_rules.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_create_table_contracts_rules extends CI_Migration
+{
+	public function up(){
+		
+		// Creamos la estructura de la nueva tabla usando la clase dbforge de Codeigniter
+		$this->dbforge->add_field(
+			array(
+				"id" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"unsigned" => TRUE,
+					"auto_increment" => TRUE,
+					"null" => FALSE
+				),
+				"var1" => array(
+					"type" => "TEXT",
+					"null" => TRUE
+				),
+				"cond" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"var2" => array(
+					"type" => "TEXT",
+					"null" => TRUE
+				),
+				"contracts_id" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"segment" => array(
+					"type" => "VARCHAR",
+					"constraint" => 150,
+					"null" => FALSE
+				),
+				"result" => array(
+					"type" => "TEXT",
+					"null" => TRUE
+				)
+			)
+		);
+		
+		$this->dbforge->add_key('id', TRUE);  // Establecemos el id como primary_key
+		
+		$this->dbforge->add_key('contracts_id');  // Establecemos la contracts_id como key
+		
+		$this->dbforge->create_table('contracts_rules', TRUE);
+		
+	}
+	
+	public function down(){
+		
+		// Eliminamos la tabla 'contracts_rules'
+		$this->dbforge->drop_table('contracts_rules', TRUE);
+		
+	}
+	
+}
