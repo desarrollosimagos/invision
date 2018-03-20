fa055dd8ecabead1089a0a3f75da641ef4333779
Jose <solorzano202009@gmail.com>
2018-03-20 11:01:32 -0400

    Nueva tabla 'projects_rules'.

---
+++ application/migrations/031_create_table_projects_rules.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_create_table_projects_rules extends CI_Migration
+{
+	public function up(){
+		
+		// Creamos la estructura de la nueva tabla usando la clase dbforge de Codeigniter
+		$this->dbforge->add_field(
+			array(
+				"id" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"unsigned" => TRUE,
+					"auto_increment" => TRUE,
+					"null" => FALSE
+				),
+				"var1" => array(
+					"type" => "TEXT",
+					"null" => TRUE
+				),
+				"cond" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"var2" => array(
+					"type" => "TEXT",
+					"null" => TRUE
+				),
+				"project_id" => array(
+					"type" => "INT",
+					"constraint" => 11,
+					"null" => TRUE
+				),
+				"segment" => array(
+					"type" => "VARCHAR",
+					"constraint" => 150,
+					"null" => FALSE
+				),
+				"result" => array(
+					"type" => "TEXT",
+					"null" => TRUE
+				)
+			)
+		);
+		
+		$this->dbforge->add_key('id', TRUE);  // Establecemos el id como primary_key
+		
+		$this->dbforge->add_key('project_id');  // Establecemos la project_id como key
+		
+		$this->dbforge->create_table('projects_rules', TRUE);
+		
+	}
+	
+	public function down(){
+		
+		// Eliminamos la tabla 'projects_rules'
+		$this->dbforge->drop_table('projects_rules', TRUE);
+		
+	}
+	
+}
